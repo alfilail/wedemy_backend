8e42176a1484680edcc7d1cc26b999ea70fc4295
Imron Rosyadi <imronxrosyadi@gmail.com>
2021-02-22 18:28:21 +0700

    Edit roles controller

--- elearning/src/main/java/com/lawencon/elearning/controller/RolesController.java
+++ elearning/src/main/java/com/lawencon/elearning/controller/RolesController.java
@@ -6,14 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -58,29 +55,6 @@ public class RolesController extends ElearningBaseController {
 		try {
 			role = rolesService.getById(id);
 			return responseSuccess(role, HttpStatus.OK, MessageStat.SUCCESS_RETRIEVE);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return responseError(e);
-		}
-	}
-
-	@PutMapping
-	public ResponseEntity<?> update(@RequestBody String body) {
-		try {
-			Roles role = new ObjectMapper().readValue(body, Roles.class);
-			rolesService.update(role);
-			return responseSuccess(role, HttpStatus.OK, MessageStat.SUCCESS_UPDATE);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return responseError(e);
-		}
-	}
-
-	@DeleteMapping
-	public ResponseEntity<?> deleteById(@RequestParam("id") String id) {
-		try {
-			rolesService.deleteById(id);
-			return responseSuccess(null, HttpStatus.OK, MessageStat.SUCCESS_DELETE);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return responseError(e);
